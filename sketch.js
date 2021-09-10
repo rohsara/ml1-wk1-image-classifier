74762517a020e2039a71b44214359721ed500934
sararo <ro.hsara@gmail.com>
2021-09-10 08:54:49 -0400

    added speech library and speech output

--- sketch.js
+++ sketch.js
@@ -2,33 +2,7 @@ let mobilenet;
 let puffin;
 let video;
 let label = '';
-
-function modelReady(){
-	console.log("Model is ready!!!");
-	// used for image classification example
-	// mobilenet.predict(puffin, getResults);
-
-	// webcam example
-	mobilenet.predict(getResults);
-}
-
-function getResults(error, results){
-	if(error){
-		console.error(error);
-	} else {
-		// console.log(results); // slow things down
-		label = results[0].label;
-		// let prob = results[0].confidence;
-		mobilenet.predict(getResults)
-		// createP(label);
-		// createP(prob);
-	}
-}
-
-// used for image classification example
-// function imageReady(){
-// 	image(puffin, 0, 0, width, height);
-// }
+const myVoice = new p5.Speech();
 
 function setup() {
 	createCanvas(640, 550);
@@ -41,7 +15,6 @@ function setup() {
 	video.hide();
 	// mobilenet = ml5.imageClassifier("MobileNet", modelReady); // image classifier ver
 	mobilenet = ml5.imageClassifier("MobileNet", video, modelReady); // webcam ver
-	
 }
 
 function draw(){
@@ -51,3 +24,37 @@ function draw(){
 	textSize(32);
 	text(label, 10, height - 20);
 }
+
+function modelReady(){
+	console.log("Model is ready!!!");
+	// used for image classification example
+	// mobilenet.predict(puffin, gotResult);
+
+	// webcam example
+	// mobilenet.predict(gotResult);
+	classifyVideo();
+}
+
+function classifyVideo(){
+	mobilenet.classify(gotResult);
+}
+
+function gotResult(error, results){
+	if(error){
+		console.error(error);
+	} else {
+		// console.log(results); // slow things down
+		label = results[0].label;
+		// let prob = results[0].confidence;
+		// mobilenet.predict(gotResult)
+		// createP(label);
+		// createP(prob);
+		myVoice.speak(`I see ${label}`);
+		classifyVideo();
+	}
+}
+
+// used for image classification example
+// function imageReady(){
+// 	image(puffin, 0, 0, width, height);
+// }
